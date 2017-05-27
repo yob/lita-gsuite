a1a9b5b5c83e9cd8d31ca2a51985e8ffae24bbc2
James Healy <james@yob.id.au>
2017-05-28 00:09:37 +1000

    remove debugging puts

--- lib/lita/commands/list_activities.rb
+++ lib/lita/commands/list_activities.rb
@@ -16,7 +16,6 @@ module Lita
       end
 
       def run(robot, target, gateway, window_start, window_end)
-        puts "run: #{window_start} #{window_end}"
         activities = gateway.admin_activities(window_start, window_end)
         activities.sort_by(&:time).map(&:to_msg).each_with_index do |message, index|
           robot.send_message(target, message)
