40b18908e9a98531abd70894c01d872f0bc8d42b
James Healy <james@yob.id.au>
2017-05-28 23:31:22 +1000

    merge EmptyGroupsMessage into Commands::EmptyGroups
    The Message class was so small, I don't think it's woth maintaining it
    outside the command class

--- lib/lita/commands/empty_groups.rb
+++ lib/lita/commands/empty_groups.rb
@@ -8,11 +8,29 @@ module Lita
       end
 
       def run(robot, target, gateway, opts = {})
-        msg = EmptyGroupsMessage.new(gateway).to_msg
+        msg = build_msg(gateway)
         robot.send_message(target, msg) if msg
         robot.send_message(target, "No groups found") if msg.nil? && opts[:negative_ack]
       end
-    end
 
+      private
+
+      def build_msg(gateway)
+        groups = empty_groups(gateway)
+
+        if groups.any?
+          msg = "The following groups have no members, which may result in undelivered email.\n"
+          msg += groups.map { |group|
+            "- #{group.email}"
+          }.join("\n")
+        end
+      end
+
+      def empty_groups(gateway)
+        gateway.groups.select { |group|
+          group.member_count == 0
+        }
+      end
+    end
   end
 end
