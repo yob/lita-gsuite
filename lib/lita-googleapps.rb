0c841976b1598186019afafa3d24dd1e193c5a52
James Healy <james@yob.id.au>
2017-05-30 23:35:41 +1000

    merge MaxWeeksSuspendedMessage into Commands::DeletionCandidates
    The two classes are so tightly coupled, I think we may as well moosh
    them together

--- lib/lita-googleapps.rb
+++ lib/lita-googleapps.rb
@@ -12,7 +12,6 @@ require "lita/window_schedule"
 
 require "lita/admin_list_message"
 require "lita/google_apps_gateway"
-require 'lita/max_weeks_suspended_message'
 require 'lita/max_weeks_without_login_message'
 require 'lita/no_org_unit_message'
 require 'lita/two_factor_message'
