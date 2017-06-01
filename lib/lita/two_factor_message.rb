4429eb74ed9914267518c00146dbbac27a443965
James Healy <james@yob.id.au>
2017-06-01 21:30:07 +1000

    optimise TwoFactorMessage
    * reduce it from two API calls to one

--- lib/lita/two_factor_message.rb
+++ lib/lita/two_factor_message.rb
@@ -5,33 +5,23 @@ module Lita
     end
 
     def to_msg
+      users = active_users
+      users_with_tfa = users.select { |user| user.two_factor_enabled? }
+      ou_paths = users.group_by(&:ou_path).keys.sort
+
       msg = "Active users with Two Factor Authentication enabled:\n\n"
-      orgunits.each do |ou|
-        msg += ou_msg(ou)
+      ou_paths.each do |ou_path|
+        msg += ou_msg(ou_path, users) + "\n"
       end
-      msg + "- Overall #{count_with_tfa}/#{count_all} (#{percentage(count_with_tfa, count_all)}%)"
+      msg + "- Overall #{users_with_tfa.size}/#{users.size} (#{percentage(users_with_tfa.size, users.size)}%)"
     end
 
     private
 
-    def ou_msg(ou)
-      ou_users = active_users.select { |user| user.ou_path == ou.path }
+    def ou_msg(ou_path, users)
+      ou_users = users.select { |user| user.ou_path == ou_path }
       with_tfa = ou_users.select { |user| user.two_factor_enabled? }
-      #with_tfa = count_ou_users_with_tfa(ou)
-      #all_users = count_ou_users(ou)
-      "- #{ou.path} #{with_tfa.size}/#{ou_users.size} (#{percentage(with_tfa.size, ou_users.size)}%)\n"
-    end
-
-    def orgunits
-      @orgunits ||= @gateway.organisational_units.sort_by(&:path)
-    end
-
-    def count_all
-      active_users.size
-    end
-
-    def count_with_tfa
-      @count_with_tfa ||= active_users.select { |user| user.two_factor_enabled? }.size
+      "- #{ou_path} #{with_tfa.size}/#{ou_users.size} (#{percentage(with_tfa.size, ou_users.size)}%)"
     end
 
     def percentage(num, denom)
